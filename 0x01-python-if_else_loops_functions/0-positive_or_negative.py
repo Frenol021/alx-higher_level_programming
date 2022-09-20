847ff9096b0d5fc44a1c6f19432aa0001fb13d69
frenol021 <frenol021@gmail.com>
2022-09-20 05:22:56 -0700

    cheking positivity

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+print(number)
+if number > 0:
+    print("it is positive")
+elif number == 0:
+    print("it is zero")
+else:
+    print("it is negative")
