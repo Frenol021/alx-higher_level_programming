176e688f9217869bf72a33d9f4974760361f1cdb
frenol021 <frenol021@gmail.com>
2022-10-13 20:43:41 -0700

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Filename:  0-square"""
+
+
 class Square:
-    "'a square is a regular quadrilateral, which means that it has four equal sides and four equal angles. It can also be defined as a rectangle with two equal-length adjacent sides "'
+    """An empty class Square that defines a square:"""
+    pass
